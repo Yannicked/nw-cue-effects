bb3c6eb0d081ed247e15072bf728accda4d3bdc0
Yannick de Jong <yannickdejong@me.com>
2016-03-28 20:50:27 +0200

    Updated

--- rebuild.bat
+++
@@ -1,7 +0,0 @@
-cd node_modules/node-win-screenres
-start "" nw-gyp rebuild %*
-cd ../../node_modules/ffi
-start "" nw-gyp rebuild %*
-cd ../../node_modules/ref
-start "" nw-gyp rebuild %*
-cd ../..
